597058336b3f69c90674b9422a0d33e2dfcec2c2
Miroslav Křenek <krenekmiroslav@gmail.com>
2018-12-01 16:24:45 +0100

    fish swimming to target

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float enemyHP = 100f;
+    [SerializeField] float enemyDamage = 10f;
+
+    private Color enemyColor;
+
+    public void GetDamage(float damage)
+    {
+        enemyHP -= damage;
+        if (enemyHP <= 0)
+        {
+            EnemyDeath();
+        }
+    }
+
+    private void EnemyDeath()
+    {
+        Destroy(this);
+    }
+}

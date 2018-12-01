3311134bab07a3a0a2e6d9839f0e115ff0471fa9
tox <tomfantadev@gmail.com>
2018-12-01 15:42:01 +0100

    Citizen spawning, basic Main scene setup

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Spawner : MonoBehaviour
+{
+
+    [SerializeField]
+    List<GameObject> spawningPoolOriginals;
+
+    [SerializeField]
+    bool oneFromPoolRandom = true;
+
+    [SerializeField]
+    int waveSize = 1;
+
+    [SerializeField]
+    GameObject spawnParent;
+
+    [SerializeField]
+    float spawnRandomRadius;
+
+    [SerializeField]
+    float spawningDelay = 1f;
+
+    [HideInInspector]
+    public List<GameObject> Spawns;
+
+    [SerializeField]
+    Vector3 spawnValues;
+
+    [SerializeField]
+    float spawnWait;
+
+    [SerializeField]
+    float startWait;
+
+    [SerializeField]
+    float waveWait;
+
+    [SerializeField]
+    int maxWaves = -1;
+
+    void Awake()
+    {
+        Spawns = new List<GameObject>();
+        spawningPoolOriginals = spawningPoolOriginals ?? new List<GameObject>();
+        spawnParent = spawnParent ?? gameObject;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(SpawnWaves());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+
+
+    }
+
+    GameObject Spawn(GameObject original)
+    {
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+        Quaternion spawnRotation = Quaternion.identity;
+      
+        GameObject SpawnInstance = Instantiate(original, spawnPosition, spawnRotation, spawnParent.transform);
+
+        Spawns.Add(SpawnInstance);
+        return SpawnInstance;
+    }
+
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            
+            for (int i = 0; i < waveSize; i++)
+            {
+                if (oneFromPoolRandom)
+                {
+                    var original = spawningPoolOriginals[Random.Range(0, spawningPoolOriginals.Count)];             
+                    Spawn(original);
+                    yield return new WaitForSeconds(spawnWait);
+                }
+                else
+                {
+                    foreach(var original in spawningPoolOriginals)
+                    { 
+                        Spawn(original);
+                        yield return new WaitForSeconds(spawnWait);
+                    }
+                }
+                
+                
+            }
+
+            yield return new WaitForSeconds(waveWait);
+        }
+    }
+
+}

212bcc971e594f1b6f3b11a37d6e0568c79c7c6a
tox <tomfantadev@gmail.com>
2018-12-02 10:50:56 +0100

    clouds rotation; Sea Waving

---
+++ Assets/Scripts/Waving.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waving : MonoBehaviour
+{
+
+    [SerializeField]
+    float speedY = 1;
+    [SerializeField]
+    float distanceY = 1;
+    [SerializeField]
+    float speedX = 1;
+    [SerializeField]
+    float distanceX = 1;
+
+    
+    void LateUpdate()
+    {
+        // Set the x position to loop between 0 and 3
+        float x = Mathf.PingPong(Time.time * speedX, distanceX);
+       
+        transform.position = new Vector3(x, transform.position.y, transform.position.z);
+    }
+}

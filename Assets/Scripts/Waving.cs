76b7eb22034abdbac0ece977170c900e7b2b7a85
tox <tomfantadev@gmail.com>
2018-12-02 11:50:31 +0100

    Started working on mimics.

--- Assets/Scripts/Waving.cs
+++ Assets/Scripts/Waving.cs
@@ -4,22 +4,30 @@ using UnityEngine;
 
 public class Waving : MonoBehaviour
 {
-
     [SerializeField]
     float speedY = 1;
     [SerializeField]
-    float distanceY = 1;
+    float distanceY = 0.001f;
     [SerializeField]
     float speedX = 1;
     [SerializeField]
     float distanceX = 1;
+    float modifierY = 1;
 
-    
+    Vector3 lastPos;
+    private void Start()
+    {
+        //lastPos = transform.position;
+    }
     void LateUpdate()
     {
+        
         // Set the x position to loop between 0 and 3
         float x = Mathf.PingPong(Time.time * speedX, distanceX);
-       
-        transform.position = new Vector3(x, transform.position.y, transform.position.z);
+        float y = transform.position.y;
+        transform.position = new Vector3(x, y, transform.position.z);
+        //modifierY *= -1;
+        //lastPos = transform.position;
+
     }
 }
